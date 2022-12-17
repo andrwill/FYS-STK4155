d29233189219175fc10723e0702c36c2a6d528d0
andrwill <andreas.willassen@gmail.com>
2022-12-17 10:15:15 +0100

    Added savefig to plots

--- Project3/symdiag_mlp.py
+++ Project3/symdiag_mlp.py
@@ -8,7 +8,7 @@ from jax import random
 
 from diffrax import diffeqsolve, ODETerm, Tsit5, SaveAt, PIDController
 
-from heat_eq_mlp import random_layers, sigmoid
+from pdemlp import random_layers, sigmoid
 
 def rayleigh_quotient(x, A):
     return x.T@A@x/(x.T@x)
@@ -95,23 +95,23 @@ if __name__ == '__main__':
     def rayleigh_qoutient(x):
         return x.T@A@x/(x.T@x)
 
-    ode_losses = jnp.linalg.norm(vmap(rhs)(ys), axis=1)
-    plt.title('ODE Loss')
-    plt.plot(ts, ode_losses)
-    plt.savefig('./figures/ode_loss.pdf')
-    plt.show()
-
     eigpair_losses = eigpair_loss(ys)
     eigval_losses = eigpair_losses[:,0]
     eigvec_losses = eigpair_losses[:,1]
     plt.title('Eigenvalue Losses')
+    plt.xlabel('$t$')
+    plt.ylabel('$|\lambda_{\max} - \lambda|$')
     plt.ylim(0.0, 1.1*jnp.max(eigval_losses))
     plt.plot(ts, eigval_losses)
+    plt.savefig('./figures/')
     plt.show()
 
     plt.title('Eigenvector Losses')
+    plt.xlabel('$t$')
+    plt.ylabel('$|\mathbf{A}\mathbf{x} - \lambda\mathbf{x}|$')
     plt.ylim(0.0, 1.1*jnp.max(eigvec_losses))
     plt.plot(ts, eigvec_losses)
+    plt.savefig('./figures/eigvec_losses.pdf')
     plt.show()
 
     num_epochs = 1000
